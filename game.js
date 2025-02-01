0c08058e0b599755423d6fb0d4a8b1a9f265719f
ssj05 <89268633+ssj05@users.noreply.github.com>
2025-02-01 11:40:08 +0530

    Add random int generator function

---
+++ game.js
@@ -0,0 +1,18 @@
+console.log("Hello, World!");
+
+function getRandomInt(n) {
+  return Math.floor(Math.random() * n);
+}
+
+// function getComputerChoice() {
+//   if (getRandomInt() === 0) {
+//     return "Rock";
+//   }else if (getRandomInt() === 1) {
+//     return "Paper";
+//   }else if (getRandomInt() === 2) {
+//     return "Scissors";
+//   }else {
+//     return "None";
+//   };
+//
+// }
