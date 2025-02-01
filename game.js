c369093169ce274a25d0aa4f1307bbee323e9d8d
ssj05 <89268633+ssj05@users.noreply.github.com>
2025-02-01 12:10:40 +0530

    add getHumanChoice()

--- game.js
+++ game.js
@@ -17,16 +17,16 @@ function getComputerChoice() {
   }
 }
 
-// function getHumanChoice() {
-//   const input = prompt("Select one:\n(r)rock\n(p)paper\n(s)scissors ");
-//   if (input.toLowerCase() == "r" || input.toLowerCase() == "rock") {
-//     return "rock";
-//   } else if (input.toLowerCase() == "p" || input.toLowerCase() == "paper") {
-//     return "paper";
-//   } else if (input.toLowerCase() == "s" || input.toLowerCase() == "scissors") {
-//     return "scissors";
-//   }
-// }
+function getHumanChoice() {
+  const input = prompt("Select one:\n(r)rock\n(p)paper\n(s)scissors ");
+  if (input.toLowerCase() == "r" || input.toLowerCase() == "rock") {
+    return "rock";
+  } else if (input.toLowerCase() == "p" || input.toLowerCase() == "paper") {
+    return "paper";
+  } else if (input.toLowerCase() == "s" || input.toLowerCase() == "scissors") {
+    return "scissors";
+  }
+}
 
 console.log(getComputerChoice());
-// console.log(getHumanChoice());
+console.log(getHumanChoice());
