38a48f12dbcbe56cf0511e16bee765e6dcb83e66
ssj05 <89268633+ssj05@users.noreply.github.com>
2025-02-01 12:09:37 +0530

    add getComputerChoice()

--- game.js
+++ game.js
@@ -3,16 +3,30 @@ console.log("Hello, World!");
 function getRandomInt(n) {
   return Math.floor(Math.random() * n);
 }
+const n = 3;
+function getComputerChoice() {
+  const randomInt = getRandomInt(n);
+  if (randomInt == 0) {
+    return "rock";
+  } else if (randomInt == 1) {
+    return "paper";
+  } else if (randomInt == 2) {
+    return "scissors";
+  } else {
+    return "None";
+  }
+}
 
-// function getComputerChoice() {
-//   if (getRandomInt() === 0) {
-//     return "Rock";
-//   }else if (getRandomInt() === 1) {
-//     return "Paper";
-//   }else if (getRandomInt() === 2) {
-//     return "Scissors";
-//   }else {
-//     return "None";
-//   };
-//
+// function getHumanChoice() {
+//   const input = prompt("Select one:\n(r)rock\n(p)paper\n(s)scissors ");
+//   if (input.toLowerCase() == "r" || input.toLowerCase() == "rock") {
+//     return "rock";
+//   } else if (input.toLowerCase() == "p" || input.toLowerCase() == "paper") {
+//     return "paper";
+//   } else if (input.toLowerCase() == "s" || input.toLowerCase() == "scissors") {
+//     return "scissors";
+//   }
 // }
+
+console.log(getComputerChoice());
+// console.log(getHumanChoice());
